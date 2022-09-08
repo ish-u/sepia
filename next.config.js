9cd70ff6948a2c2c06527aa1dc742c5cb9521680
ish-u <anmolgupta520@gmail.com>
2022-09-09 00:55:55 +0530

    added remotePatterns

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "**.spotifycdn.com",
+      },
+    ],
     domains: ["i.scdn.co", "images.unsplash.com"],
   },
 };
