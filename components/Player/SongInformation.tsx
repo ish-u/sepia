cfe8e19efe9c4d85f6d3cd87b044c33d0af00950
ish-u <anmolgupta520@gmail.com>
2022-09-10 12:51:12 +0530

    added links

--- components/Player/SongInformation.tsx
+++ components/Player/SongInformation.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const SongInformation = ({ track }: { track: Spotify.Track }) => {
   return (
@@ -9,14 +10,24 @@ const SongInformation = ({ track }: { track: Spotify.Track }) => {
           src={track?.album.images[0].url || ""}
           height={track?.album.images[1].height || 64}
           width={track?.album.images[1].width || 64}
+          layout="fixed"
         ></Image>
       </div>
       <div className="m-2 flex flex-col justify-center">
-        <div className="font-bold text-lg hover:underline hover:cursor-pointer">
-          {track?.name}
+        <div className="font-bold text-lg hover:underline hover:cursor-pointer line-clamp-1">
+          <Link href={`/album/${track.album.uri.split(":").at(-1)}`}>
+            {track?.name || "SOME SONG"}
+          </Link>
         </div>
-        <div className="text-sm ">
-          {track?.artists?.map((artist) => artist.name + " ")}
+        <div className="text-sm hover:underline">
+          {track?.artists?.map((artist) => (
+            <Link
+              key={artist.name}
+              href={`/artist/${artist.uri.split(":").at(-1)}`}
+            >
+              {artist.name + " "}
+            </Link>
+          ))}
         </div>
       </div>
     </div>
