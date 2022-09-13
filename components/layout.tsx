3ca9bbb00d0e0c070ef2f2a9988304bf3908ea58
ish-u <anmolgupta520@gmail.com>
2022-09-14 00:54:32 +0530

    moved getUserQueue to queue page; linter fixes

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,56 +8,28 @@ import { AppContext } from "../context/context";
 import { ActionType } from "../context/actions";
 
 export default function Layout({ children }: { children: ReactElement }) {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
   const { state, dispatch } = useContext(AppContext);
 
-  const getCurrentUser = async () => {
-    const response = await fetch("/api/spotify/user/me");
-    if (response.status === 200) {
-      const currentUser: SpotifyUser = await response.json();
-      dispatch({ type: ActionType.SetUser, payload: { user: currentUser } });
-    }
-  };
-
-  const getUserQueue = async () => {
-    const res = await fetch("/api/spotify/player/queue");
-    const userQueue = (await res.json()).data;
-    if (res.status === 200) {
-      dispatch({
-        type: ActionType.UpdateQueue,
-        payload: {
-          queue: userQueue,
-        },
-      });
-    }
-  };
-
   useEffect(() => {
+    const getCurrentUser = async () => {
+      const response = await fetch("/api/spotify/user/me");
+      if (response.status === 200) {
+        const currentUser: SpotifyUser = await response.json();
+        dispatch({ type: ActionType.SetUser, payload: { user: currentUser } });
+      }
+    };
     if (state.user === undefined && status === "authenticated") {
       getCurrentUser();
-      getUserQueue();
-      const fetchData = setInterval(() => {
-        if (session?.accessToken) {
-          getUserQueue();
-          getCurrentUser();
-        }
-      }, 5000);
-      () => {
-        clearInterval(fetchData);
-      };
     }
-  }, [status]);
-
-  useEffect(() => {
-    console.log("RENDER");
-  }, [state.queue]);
+  }, [state.user, status, dispatch]);
 
   return (
     <>
       <div className="fixed top-0 left-0 h-full w-full -z-50  bg-gradient-to-b from-[#FFDCCC] to-slate-400"></div>
-      {<NavBar />}
+      <NavBar />
       {children}
-      {<Player />}
+      <Player />
     </>
   );
 }
