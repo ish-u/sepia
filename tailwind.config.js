d69ba77f308f56946f15128941f67317bceb4973
ish-u <anmolgupta520@gmail.com>
2022-10-16 19:43:42 +0530

    added gradient animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,24 @@ module.exports = {
           "0%": { transform: "scale(0)" },
           "100%": { transform: "scale(1)" },
         },
+        gradient: {
+          "0% ": {
+            "background-size": "300% 300%",
+            "background-position": "10% 0%",
+          },
+          "50%": {
+            "background-size": "300% 300%",
+            "background-position": "91% 100%",
+          },
+          "100%": {
+            "background-size": "300% 300%",
+            "background-position": "10% 0%",
+          },
+        },
       },
       animation: {
         appear: "appear 150ms",
+        gradient: "gradient 10s ease infinite",
       },
     },
   },
