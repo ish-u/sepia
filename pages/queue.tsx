cefec902b895bbb34c9e35dfd3aa36615b0b9d8e
Anmol Gupta <anmolgupta520@gmail.com>
2023-05-07 17:57:51 +0530

    feat: remove AppContext and use useSepiaStore

--- pages/queue.tsx
+++ pages/queue.tsx
@@ -1,48 +1,44 @@
-import { useContext, useEffect } from "react";
+import { useEffect } from "react";
 import { Track } from "../components/TrackList";
-import { AppContext } from "../context/context";
-import { ActionType } from "../context/actions";
+import { useSepiaStore } from "../store/store";
 
 const Queue = () => {
-  const { state, dispatch } = useContext(AppContext);
+  const track = useSepiaStore((state) => state.track);
+  const queue = useSepiaStore((state) => state.queue);
+  const setQueue = useSepiaStore((state) => state.setQueue);
 
   useEffect(() => {
     const getUserQueue = async () => {
       const res = await fetch("/api/spotify/player/queue");
       const userQueue = (await res.json()).data;
       if (res.status === 200) {
-        dispatch({
-          type: ActionType.UpdateQueue,
-          payload: {
-            queue: userQueue,
-          },
-        });
+        setQueue(userQueue);
       }
     };
 
     getUserQueue();
-  }, [state.track, dispatch]);
+  }, [track]);
 
-  useEffect(() => {}, [state.track, state.queue]);
+  useEffect(() => {}, [track, queue]);
 
   return (
     <div className="mx-36 p-8">
       <div className="text-4xl">Queue</div>
       <div>
         <div className="text-black/75 font-semibold mt-8 mb-4">Now Playing</div>
-        {state.queue?.currently_playing && (
+        {queue?.currently_playing && (
           <Track
             showArtist={true}
             showIdx={true}
-            track={state.queue.currently_playing}
-            key={state.queue.currently_playing.id}
+            track={queue.currently_playing}
+            key={queue.currently_playing.id}
             idx={1}
           />
         )}
         <div className="text-black/75 font-semibold my-4">Next Up</div>
 
-        {state.queue &&
-          state.queue?.queue.map((track, idx) => {
+        {queue &&
+          queue?.queue.map((track, idx) => {
             return (
               <Track
                 showArtist={true}
